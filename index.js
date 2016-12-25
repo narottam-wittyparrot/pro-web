9a9dc32b57a2a4a27903ef38e56693d12bba605d
Narottam Dattatrey <dattatrey93@gmail.com>
2016-12-26 00:50:15 +0530

    firebase continue

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 (function() {
     'use strict';
-    var WebApp = angular.module('WebApp', []);
+    var WebApp = angular.module('WebApp', ['firebase']);
     WebApp.controller('ContactController', ContactController);
 
     /** ngInject */
     function ContactController() {
+        var defaultApp = firebase.initializeApp(defaultAppConfig);
+
+        console.log(defaultApp.name); // "[DEFAULT]"
+        // var ref = firebase.database().ref().child("data");
+        // // download the data into a local object
+        // var syncObject = $firebaseObject(ref);
+        // // synchronize the object with a three-way data binding
+        // // click on `index.html` above to see it used in the DOM!
+        // syncObject.$bindTo($scope, "data");
+
+
         var vm = this;
+        vm.blogPost = {
+
+        }
         vm.contactDetails = [{
             "login": "dattatrey93",
             "avatar_url": "https://avatars.githubusercontent.com/u/13445492?v=3",
@@ -87,5 +101,8 @@
             "followers": 3,
             "following": 0,
         }];
+
+        console.log(firebase);
     };
+
 })();
